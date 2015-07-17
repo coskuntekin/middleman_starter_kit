2c82eb75ccff390aabbc83697f4b12a854b828ab
coskuntekin <mail@coskuntekin.com>
2015-07-17 10:54:43 +0800

    changed css comment title, added custom font, rename the themes file, added js components

--- source/assets/javascripts/main.js
+++ source/assets/javascripts/main.js
@@ -1,1 +1,16 @@
-//= require bootstrap-sprockets
+//------------------------------------
+//	#BOOTSTRAP JS COMPONENTS
+//------------------------------------
+
+//= require bootstrap/transition
+//= require bootstrap/modal
+//= require bootstrap/dropdown
+//= require bootstrap/scrollspy
+//= require bootstrap/tab
+//= require bootstrap/tooltip
+//= require bootstrap/popover
+//= require bootstrap/alert
+//= require bootstrap/button
+//= require bootstrap/collapse
+//= require bootstrap/carousel
+//= require bootstrap/affix
